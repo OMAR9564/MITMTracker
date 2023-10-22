126d37b3205cbacd835a980f5461b35b01360a62
Ömer Faruk <teknopluse12@gmail.com>
2023-10-22 16:32:46 +0300

    fix bug in get_gateway_ip fun and add new ip patern controller

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
+# Author      : OMAR ALFAROUK ALMOHAMAD
+
 import re
 import time
-import scapy.all as scapy
 import platform
 import subprocess
 from colorama import Fore, Style
-import nmap
 import uuid
-import socket
 import netifaces
 from getmac import get_mac_address
 
+#control if ip is valid because sometimes next to the interface unwanted data
+def control_ip(ip):
+    pattern = r"^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$"
 
-def get_gateway_ip():
+    if re.match(pattern, ip):
+        return True
+    else:
+        return False
+
+
+#use netifaces to get getway local ip address
+def get_gateway_ip(interface_name):
     try:
         gateway_ip = None
         gateways = netifaces.gateways()
-        if 'default' in gateways:
-            gateway_ip = gateways['default'][netifaces.AF_INET][0]
+        for gateway in gateways:
+            if gateway == "default":
+                if gateways[gateway][netifaces.AF_INET][1] == interface_name and control_ip(gateways[gateway][netifaces.AF_INET][0]):
+                    gateway_ip = gateways[gateway][netifaces.AF_INET][0]
+                    break
+                else:
+                    continue
+            elif gateway != "default":
+                if gateways[gateway][0][1] == interface_name and control_ip(gateways[gateway][0][0]):
+                    gateway_ip = gateways[gateway][0][0]
+                    break
+                else:
+                    continue
+            else:
+                continue
+
         return gateway_ip
 
     except Exception as e:
-        print(f'Hata oluştu: {e}')
+        print(f'Error: {e}')
         return None
 
 
-def searchMacAddress(data):
-    mac_address_search = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", data)
-    if mac_address_search:
-        return mac_address_search.group(0)
-    return None
-
-
-def findMacAddress(interfaceName):
+def find_mac_address(interface_name):
     try:
         if platform.system() == "Darwin" or platform.system() == "Linux":
             try:
-                default_gateway_ip = get_gateway_ip()
+                default_gateway_ip = get_gateway_ip(interface_name)
                 if default_gateway_ip:
                     arp_result = subprocess.check_output(["arp", "-a"]).decode("utf-8")
                     lines = arp_result.splitlines()
@@ -50,50 +66,50 @@ def findMacAddress(interfaceName):
                 return None
 
         elif platform.system() == "Windows":
-            if interfaceName == " " or interfaceName is None:
+            if interface_name == " " or interface_name is None:
                 mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
                 return mac_address
             else:
                 result = subprocess.check_output(["getmac"], universal_newlines=True)
                 lines = result.split("\n")
                 for line in lines:
-                    if interfaceName in line:
+                    if interface_name in line:
                         mac_address = line.split()[0]
                         return mac_address
         else:
-            raise Exception("Bu işletim sistemi desteklenmiyor.")
+            raise Exception("This operating system is not support.")
     except Exception as e:
-        print(f"Hata oluştu: {e}")
+        print(f"Error: {e}")
         return None
 
-
-def getMacAddressAlternative(interface):
-    routerIp = get_gateway_ip()
-    macAddress = get_mac_address(interface, routerIp)
-    return macAddress
+#this function is get mac address faster then arp requesting
+def get_mac_address_alternative(interface):
+    router_ip = get_gateway_ip(interface)
+    mac_address = get_mac_address(interface, router_ip)
+    return mac_address
 
 
 orjinal_mac = input("Please enter the MAC address(00:11:22:33:44:55):\n")
 changed = False
-interfaceName = None
+interface_name = None
 if platform.system() == "Darwin" or platform.system() == "Linux":
-    interfaceName = input("Please enter the interface name (etc. en0)\nPress enter for wlan0:")
-    if interfaceName == "":
-        interfaceName = "wlan0"
+    interface_name = input("Please enter the interface name (etc. en0)\nPress enter for wlan0:")
+    if interface_name == "":
+        interface_name = "wlan0"
 
-trackerType = None
-while trackerType != '1' and trackerType != '2':
-    trackerType = input("Please select a method of tracker\n1.Scan with arp\n2.Scan with get-mac lab.\n")
+method_type = None
+while method_type != '1' and method_type != '2':
+    method_type = input("Please select a method of tracker\n1.Scan with arp\n2.Scan with get-mac lab.\n")
 
-print(f"\n\nMevcut Wi-Fi MAC adresi: {orjinal_mac}")
+print(f"\n\nCurrent MAC address: {orjinal_mac}")
 
 while True:
     try:
         current_mac = None
-        if trackerType == '1':
-            current_mac = findMacAddress(interfaceName)
-        elif trackerType == '2':
-            current_mac = getMacAddressAlternative(interfaceName)
+        if method_type == '1':
+            current_mac = find_mac_address(interface_name)
+        elif method_type == '2':
+            current_mac = get_mac_address_alternative(interface_name)
 
         if current_mac != orjinal_mac:
             if current_mac is not None:
@@ -110,5 +126,5 @@ while True:
         break
 
     except Exception as e:
-        print(f"Hata oluştu: {e}")
+        print(f"Error: {e}")
         time.sleep(5)
